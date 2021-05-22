1678d281df378f272babbeb2002ae630f0caa30d
eWave-Ziggy <leon@ewave-esports.de>
2021-05-22 08:21:04 +0200

    update to ticket-system

--- ticket-system/__init__.py
+++ ticket-system/__init__.py
@@ -1,7 +1,7 @@
-from .reacttickets import ReactTickets
+from .Ticket-System import Ticket-System
 
 
 async def setup(bot):
-    cog = ReactTickets(bot)
+    cog = Ticket-System(bot)
     bot.add_cog(cog)
     await cog.initialize()
